b15c3afb7a6a159cab0ee87a9441e15260da23af
Stephen Wilkinson <du_monde@hotmail.com>
2014-07-30 14:33:51 +0000

    [Core] CMS page autoloading, woohooo!

--- Repository/PageRepository.php
+++ Repository/PageRepository.php
@@ -53,10 +53,57 @@ class PageRepository extends EntityRepository
 	 */
 	public function estimate($path)
 	{
-		// load the relevant CMS page from the database
-		// @todo - make the query to estimate
-		$page = null;
+		// segment the uri for matching
+		$segments = explode('/', $path);
 
-		return $page;
+		// if we have no segements then use the raw path, as it only contains one segment
+		!$segments and $segments = array($path);
+
+		// segment in % for the SQL like comparison
+		array_walk($segments, function(&$item) {
+			$item = '%'.$item.'%';
+		});
+
+		// consider all pages that have least one segment in their slug as suitable candidates
+		$qb = $this->getEntityManager()->createQueryBuilder();
+
+		$qb->addSelect('page');
+		$qb->from('StemsPageBundle:Page', 'page');
+
+		$qb->where('page.deleted = :deleted');
+		$qb->setParameter('deleted', '0');
+
+		// we need a like clause for each as they cannot be done via an array
+		$segmentLikes = array();
+
+		foreach ($segments as $i => $segment) {
+			$qb->setParameter('segment'.$i, $segment);
+			$segmentLikes[] = $qb->expr()->like('page.slug', ':segment'.$i);
+		}
+		
+		// bosh them all into an orX so we bracket them off from the delete clause
+		$qb->andWhere($qb->expr()->orX()->addMultiple($segmentLikes));
+
+		$candidates = $qb->getQuery()->getResult();
+
+		// run comparison for each matched page to see if they're suitable	
+		foreach ($candidates as $candidate) {
+			
+			// replace the dynamic components from the page's slug (eg. {id}) with the wildcard character
+			$pattern = trim(preg_replace('/\s*\{[^)]*\}/', '*', $candidate->getSlug()));
+
+			// run a the match function to compare the path against the wildcarded slug
+			$pattern = preg_quote($pattern,'/');        
+		    $pattern = str_replace( '\*' , '.*', $pattern);   
+
+		    if (preg_match('/^'.$pattern.'$/i', $path)) {
+		    	return $candidate;
+		    }
+		}
+
+		die();
+
+		// fallback if no matches found
+		return null;
 	}
 }
