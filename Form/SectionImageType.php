39fb7b0e5e8215fc8bf7f4dbd6df161e9b17807e
ste-wilkinson <du_monde@hotmail.com>
2016-06-16 22:46:25 +0100

    Added portrait layout option for image section

--- Form/SectionImageType.php
+++ Form/SectionImageType.php
@@ -18,45 +18,51 @@ class SectionImageType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-		$builder->add('image', 'hidden', array(
+		$builder->add('image', 'hidden', [
 			'label'     		=> 'Image ID',
 			'required'			=> false,
 			'error_bubbling' 	=> true,
-		));	
+		]);
 
-		$builder->add('position', 'choice', array(
-			'label'     		=> 'Positioning',
+		$builder->add('position', 'choice', [
+			'label'     		=> 'Layout Style',
 			'empty_value' 		=> false,
-			'choices'			=> array('squared' => 'Squared', 'full-width' => 'Full Width'),
+			'choices'			=> [
+				'squared'    => 'Squared',
+				'portrait'   => 'Portrait',
+				'full-width' => 'Full Width'
+			],
 			'required'			=> false,
 			'error_bubbling' 	=> true,
-		));
+		]);
 
-	    $builder->add('upload', new EmbeddedImageType('blog'), array(
+	    $builder->add('upload', new EmbeddedImageType('blog'), [
 		    'label'     		=> 'Image File',
 		    'required'			=> false,
 		    'error_bubbling' 	=> true,
 		    'mapped'            => false,
-	    ));
+	    ]);
 
-	    $builder->add('caption', 'text', array(
+	    $builder->add('caption', 'text', [
 			'label'     		=> 'Caption (Optional)',
 			'required'			=> false,
 			'error_bubbling' 	=> true,
-		));
+		]);
 
-	    $builder->add('effect', 'text', array(
+	    $builder->add('effect', 'text', [
 		    'label'     		=> 'Image Effect (Optional)',
 		    'required'			=> false,
 		    'error_bubbling' 	=> true,
-		    'attr'              => array('class' => 'section-effect')
-	    ));
+		    'attr'              => [
+			    'class' => 'section-effect'
+		    ]
+	    ]);
 
-	    $builder->add('link', 'text', array(
+	    $builder->add('link', 'text', [
 			'label'     		=> 'Link (Optional)',
 			'required'			=> false,
 			'error_bubbling' 	=> true,
-		));	
+		]);
 	}
 
 	public function getName()
